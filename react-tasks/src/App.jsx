8b4196c70f213e2392f0eae24dfca2ad3b20c48f
Liseth Moreno <lisethpatriciamorenomarrugo@gmail.com>
2022-11-02 10:54:38 -0500

    actualizacion

--- react-tasks/src/App.jsx
+++ react-tasks/src/App.jsx
@@ -1,17 +1,99 @@
-import TaskList from './components/TaskList';
-import TaskForm from './components/TaskForm';
+import { useEffect, useMemo, useState } from "react"
 
+import { Form } from './components/Form'
+import { Input } from "./components/Input"
+import { Tasks } from './components/Tasks'
 
+import styles from './styles/app.module.css'
 
-function App(){
+const LOCALSTORAGE_TASKS_KEY = 'todolist-tasks'
+
+export function App() {
+  const [tasks, setTasks] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [searchTaskName, setSearchTaskName] = useState('')
+
+  const onAddTask = (newTask) => {
+    setTasks(currentState => [...currentState, newTask])
+    setSearchTaskName('')
+  }
+
+  const onRemoveTask = (taskId) => {
+    setTasks(currentState => currentState.filter(task => task.id !== taskId))
+  }
+
+  const onChangeCompleted = (taskId) => {
+    const taskIndex = tasks.findIndex(task => task.id === taskId)
+
+    const updatedTask = [...tasks]
+    updatedTask[taskIndex].completed = !updatedTask[taskIndex].completed
+    
+    setTasks(updatedTask)
+  }
+
+  // Esse bloco de código é disparado toda a vez que o array de
+  // tasks sofrer alguma alteração(add, remove, update)
+  useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem(LOCALSTORAGE_TASKS_KEY, JSON.stringify(tasks))
+    }
+  }, [tasks])
+
+  // Esse bloco de código é disparado ao carregar a página do usuário
+  useEffect(() => {
+    const tasksLocal = localStorage.getItem(LOCALSTORAGE_TASKS_KEY)
+    tasksLocal && setTasks(JSON.parse(tasksLocal))
+    setIsLoading(false)
+  }, [])
+
+  const handleTermSearch = (e) => {
+    const valueTerm = e.target.value.toLocaleLowerCase()
+    setSearchTaskName(valueTerm)
+  }
+
+  const totalTasks = useMemo(() => {
+    return tasks.length
+  }, [tasks])
+
+  const totalCompletedTasks = useMemo(() => {
+    return tasks.filter(task => task.completed).length
+  })
 
   return (
-    <>
-      <TaskForm />
-      <TaskList />
-    
-    </>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>TODOLIST</h1>
+
+        <Form onSubmit={onAddTask} />
+
+        <hr />
+
+        <Input
+          type="text"
+          value={searchTaskName}
+          onChange={handleTermSearch}
+          placeholder="Buscar una tarea"
+        />
+
+        <Tasks
+          tasks={tasks}
+          searchTaskName={searchTaskName}
+          onRemoveTask={onRemoveTask}
+          onChangeCompletedTask={onChangeCompleted}
+        />
+
+        <footer className={styles.footer}>
+          <h6>
+            Total de tareas:
+            <span>{totalTasks}</span>
+          </h6>
+
+          <h6>
+            Total de tareas realizadas:
+            <span>{totalCompletedTasks}</span>
+          </h6>
+        </footer>
+      </div>
+    </div>
   );
 }
-
-export default App
