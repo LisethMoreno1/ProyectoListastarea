2f8633af225ce896c16dd88f96db8e8d870900a2
Liseth Moreno <lisethpatriciamorenomarrugo@gmail.com>
2022-11-03 11:58:16 -0500

    Revert "actualizacion"
    This reverts commit 0b4ff46d35148e5196a7e90abc25b566fdcf4d53.

--- react-tasks/src/App.jsx
+++ react-tasks/src/App.jsx
@@ -1,68 +1,95 @@
 import { useEffect, useMemo, useState } from "react"
-
-import { Form } from './components/Form'
-import { Input } from "./components/Input"
-import { Tasks } from './components/Tasks'
-
+import { Form } from './components/Form/Form.index'
+import { Input } from "./components/Input/Input.index"
+import { Tasks } from './components/Tasks/Tasks.index'
+import { FaTrashAlt } from "react-icons/fa";
 import styles from './styles/app.module.css'
 
 const LOCALSTORAGE_TASKS_KEY = 'todolist-tasks'
 
 export function App() {
-  const [tasks, setTasks] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [searchTaskName, setSearchTaskName] = useState('')
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTaskName, setSearchTaskName] = useState("");
 
+
+
+  // AGREGAR ELEMENTOS A LA LISTA 
   const onAddTask = (newTask) => {
-    setTasks(currentState => [...currentState, newTask])
-    setSearchTaskName('')
-  }
-
-  const onRemoveTask = (taskId) => {
-    setTasks(currentState => currentState.filter(task => task.id !== taskId))
-  }
-
-  const onChangeCompleted = (taskId) => {
-    const taskIndex = tasks.findIndex(task => task.id === taskId)
-
-    const updatedTask = [...tasks]
-    updatedTask[taskIndex].completed = !updatedTask[taskIndex].completed
+    setTasks((currentState) => [...currentState, newTask]);
+    setSearchTaskName("");
     
-    setTasks(updatedTask)
-  }
+  };
 
-  // Esse bloco de código é disparado toda a vez que o array de
-  // tasks sofrer alguma alteração(add, remove, update)
+
+  
+// ELIMINAR ELEMENTO DE LA LISTA
+  const onRemoveTask = (taskId) => {
+    setTasks((currentState) =>
+      currentState.filter((task) => task.id !== taskId)
+    );
+  };
+
+
+  // EDITAR ELEMENTOS DE LA LISTA
+  const onEditTask = () => {
+   
+  };
+
+
+  
+  // ELIMINAR TODO LOS ELEMENTOS DE LA LISTA 
+ function onDeleteAllTask() {
+   setTasks([]);
+ }
+  
+
+
+//  ELEMENTO QUE ESTAN COMPLETADOS 
+  const onChangeCompleted = (taskId) => {
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    const updatedTask = [...tasks];
+    updatedTask[taskIndex].completed = !updatedTask[taskIndex].completed;
+
+    setTasks(updatedTask);
+  };
+
+  // Este bloque de código se dispara cada vez que el
+  // cambios de tareas (añadir, eliminar, actualizar)
   useEffect(() => {
-    if(!isLoading) {
-      localStorage.setItem(LOCALSTORAGE_TASKS_KEY, JSON.stringify(tasks))
+    if (!isLoading) {
+      localStorage.setItem(LOCALSTORAGE_TASKS_KEY, JSON.stringify(tasks));
     }
-  }, [tasks])
+  }, [tasks]);
 
-  // Esse bloco de código é disparado ao carregar a página do usuário
+  // Este bloque de código se dispara al cargar la página del usuario
   useEffect(() => {
-    const tasksLocal = localStorage.getItem(LOCALSTORAGE_TASKS_KEY)
-    tasksLocal && setTasks(JSON.parse(tasksLocal))
-    setIsLoading(false)
-  }, [])
+    const tasksLocal = localStorage.getItem(LOCALSTORAGE_TASKS_KEY);
+    tasksLocal && setTasks(JSON.parse(tasksLocal));
+    setIsLoading(false);
+  }, []);
 
   const handleTermSearch = (e) => {
-    const valueTerm = e.target.value.toLocaleLowerCase()
-    setSearchTaskName(valueTerm)
-  }
+    const valueTerm = e.target.value.toLocaleLowerCase();
+    setSearchTaskName(valueTerm);
+  };
 
+  // tOTAL DE TAREAS EN LA LISTA 
   const totalTasks = useMemo(() => {
-    return tasks.length
-  }, [tasks])
+    return tasks.length;
+  }, [tasks]);
 
+
+  // TOTAL DE TAREAS REALIZADAS
   const totalCompletedTasks = useMemo(() => {
-    return tasks.filter(task => task.completed).length
-  })
+    return tasks.filter((task) => task.completed).length;
+  });
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>TODOLIST</h1>
+        <h1>Lista De Tareas</h1>
 
         <Form onSubmit={onAddTask} />
 
@@ -79,6 +106,8 @@ export function App() {
           tasks={tasks}
           searchTaskName={searchTaskName}
           onRemoveTask={onRemoveTask}
+          onEditTask={onEditTask}
+          onDeleteAllTask={onDeleteAllTask}
           onChangeCompletedTask={onChangeCompleted}
         />
 
@@ -91,6 +120,13 @@ export function App() {
           <h6>
             Total de tareas realizadas:
             <span>{totalCompletedTasks}</span>
+          </h6>
+          <h6
+            type="button"
+            className={styles.footer}
+            onClick={() => onDeleteAllTask(id, name)}
+          >
+            <FaTrashAlt size={16} />
           </h6>
         </footer>
       </div>
