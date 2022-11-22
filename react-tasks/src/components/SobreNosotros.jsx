209f2a8ab4b1138afbb21e62c6975b29f3e92fc0
Liseth Moreno <lisethpatriciamorenomarrugo@gmail.com>
2022-11-22 17:31:52 -0500

    MODO OSCURO USANDO CHAKRA

--- react-tasks/src/components/SobreNosotros.jsx
+++ react-tasks/src/components/SobreNosotros.jsx
@@ -1,31 +1,65 @@
 import React from 'react'
-
+import { ChakraProvider, Heading, extendTheme } from "@chakra-ui/react";  
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { MdCheckCircle } from "react-icons/md";
+import { MdSettings } from "react-icons/md";
+import { AiFillGithub } from "react-icons/ai";
 export default function SobreNosotros() {
   return (
     <>
-      <h1>Sobre nosotros</h1>
-      <h3 style={{ color: "black", textAlign: "center" }}>Mi lista de Tarea</h3>
+      <Heading>Sobre nosotros</Heading>
+      <Heading as="h4" size="md" style={{ textAlign: "center" }}>
+        Mi lista de Tarea
+      </Heading>
       <hr />
-      <p style={{ color: "black" }}>
+      <p>
         Mi Lista de tarea es un sistema que pretente ayudarte en procesos
         rutinarios los cual deseas agendar para recordar en el futuro.
         <br />
         Es un sistema que pretende ser interactivo con el usuario para asi
         facilitar la experiencia de el usuario con una bonita interfaz.{" "}
       </p>
-      <ul style={{ color: "black" }}>
-        Cararteristicas que ofrece:
-        <br />
-        <li>Crear Tareas sin limites</li>
-        <li>Buscar Tareas que hayas realizado</li>
-        <li>Tachar tareas Realizadas</li>
-        <li>Eliminar tareas</li>
-        <li>Y más Actualizaciones en el Furuto</li>
-      </ul>
-      <p>Desarrollador : Liseth Moreno</p>
+      <br />
+      <List spacing={3}>
+        <p>Funciones que puedes encontrar</p>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color="green.500" />
+          Crear Tareas sin limites
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color="green.500" />
+          Buscar Tareas que hayas realizado
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color="green.500" />
+          Eliminar tareas
+        </ListItem>
+        {/* You can also use custom icons from react-icons */}
+        <ListItem>
+          <ListIcon as={MdSettings} color="green.500" />Y más Actualizaciones en
+          el Furuto
+        </ListItem>
+      </List>
 
-      <p style={{ textAlign: "center" }}>Redes Sociales</p>
-      <a href="https://github.com/LisethMoreno1">GITHUB</a>
+      <br />
+      <hr />
+      <div>
+        <p>Desarrollador : Liseth Moreno</p>
+        <p>
+          Redes Sociales
+          <a href="https://github.com/LisethMoreno1">
+            <AiFillGithub />
+            Github
+          </a>
+        </p>
+      </div>
     </>
   );
 }
--- react-tasks/src/menu.jsx
+++ react-tasks/src/menu.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { ChakraProvider, Heading, extendTheme } from "@chakra-ui/react";
+
+
+
+
 export default function Menu () {
+  const theme = extendTheme({
+    config: {
+      useSystemColorMode: false,
+      initialColorMode: "dark",
+    },
+  });
+
+
   return (
-    <div>
-      <nav>
-        <h1>Principal</h1>
-        <Link to="/" className="navLi">
-          Home
-        </Link>
-        <br />
-        <Link to="/Inicio" className="navLi">
-          Tareas
-        </Link>
-        <br />
-        <Link to="/SobreNosotros" className="navLi">
-          Sobre Nosotros
-        </Link>
-      </nav>
-    </div>
-    
+    <ChakraProvider theme={theme}>
+      <div>
+        <Heading style={{ textAlign: "center" }}>Principal</Heading>
+        <nav style={{ display: "flex", justifyContent: "space-between" }}>
+          <Link to="/" className="navLi">
+            Home
+          </Link>
+          <br />
+          <Link to="/Inicio" className="navLi">
+            Tareas
+          </Link>
+          <br />
+          <Link to="/SobreNosotros" className="navLi">
+            Sobre Nosotros
+          </Link>
+        </nav>
+      </div>
+    </ChakraProvider>
   );
 }
